e0f9ea90e88138e88543a7961f121b8c7cfba334
Italo Alves <italo.alves.jp@gmail.com>
2021-04-17 04:01:46 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/zup/addressmanagement/AddressManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.addressmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/addressmanagement/AddressManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.addressmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
